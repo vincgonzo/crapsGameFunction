8d417646b80c87c88203a32713d844f7697b0157
vincgonzo <vincgonzo@hotmail.fr>
2021-03-05 12:27:40 +0100

    while conditions rewrite

--- src/crapsGame/crapsFunctions.java
+++ src/crapsGame/crapsFunctions.java
@@ -45,27 +45,36 @@ public class crapsFunctions {
                 System.out.println("Vous n'avez pas assez de jetons. Recommencez la mise");
             else
                 System.out.println("Vous misez ====== > " + m);
-        }while (m > 0 && reserve < m);
+        } while (m != 0 && !(m < reserve));
         return m;
     }
 
-    public static boolean joueEtGagneUnTourDeCraps(){
-        int score = 0, tour = 0, dice1, dice2, j = 0, goal;
-        boolean back = false;
+    public static void loss(int tokens, int mise){
+        tokens -= mise;
+        return;
+    }
+
+    public static void win(int tokens, int mise){
+        tokens += (mise * 2);
+        return;
+    }
+
+    public static void joueEtGagneUnTourDeCraps(int tokens, int mise){
+        int score = 0, dice1, dice2, goal = 0;
         do {
             dice1 = roll();
             dice2 = roll();
             diceDisplay(dice1, dice2);
             score = dice1 + dice2;
-            if (tour == 0) {
-                if (score == 7 || score == 11) {
-                    System.out.println("Vous avez fait 7 ou 11 au premier coup. Vous avez gagné le gros lot !!");
-                    back = true;tour++;// bricolage ?
-                }
-                else if (score == 2 || score == 3 || score == 12) {
-                    System.out.println("Mauvaise pioche pour démarrer, perdu !!!!");back = true;
-                }
-            } else {
+            if (score == 7 || score == 11) {
+                System.out.println("Vous avez fait 7 ou 11 au premier coup. Vous avez gagné le gros lot !!");
+                win(tokens, mise);
+                goal = score;
+            }
+            else if (score == 2 || score == 3 || score == 12) {
+                System.out.println("Mauvaise pioche pour démarrer, perdu !!!!");
+                goal = score;
+            }else {
                 goal = score;
                 System.out.println("Maintenant l'objectif est : " + goal);
                 do {
@@ -74,36 +83,29 @@ public class crapsFunctions {
                     diceDisplay(dice1, dice2);
                     score = dice1 + dice2;
 
-                    if (score == 7) {
-                        System.out.println("You make a 7 and you Loss Man. Try Again.");back = true;
-                    }else if (score == goal) {
-                        System.out.println("Vous avez gagné, vous doublé votre mise..");back = true;
+                    if (score == goal) {
+                        System.out.println("Vous avez gagné, vous doublé votre mise..");
+                        win(tokens, mise);
                     }
-                    System.out.println( "Value goal ==> " + goal);
-                    System.out.println( "Value score ==> " + score);
-                }while (back != true);
+                    if (score == 7) {
+                        System.out.println("You make a 7 and you Loss Man. Try Again.");
+                        loss(tokens, mise);
+                        goal = score;
+                    }
+                }while (score != goal);
             }
-            tour++;
-            System.out.println("value back === > " + back);
-        }while (back != true);
-        return back;
+        }while (score != goal);
+        return;
     }
 
     public static void startGame(){
         int tokens = 10, mise = 0;
         String answer;
         Scanner s = new Scanner(System.in);
-        boolean res = true;
         System.out.println("======================== GAME START ==============================");
         do {
             mise = bet(tokens);
-            res = joueEtGagneUnTourDeCraps();
-            if(res) {
-                tokens += (mise * 2);
-            }
-            else {
-                tokens -= mise;
-            }
+            joueEtGagneUnTourDeCraps(tokens, mise);
             answer = askGamer();
             System.out.println("answer == " + answer);
         }while (mise != 0 && !(answer.equals("N")) && (tokens != 0));
