3a83f0a8e945392c2c84cbdebc2feca358405fad
vincgonzo <vincgonzo@hotmail.fr>
2021-03-05 11:11:38 +0100

    rewrite for startGame and for gameModel logik

--- src/crapsGame/crapsFunctions.java
+++ src/crapsGame/crapsFunctions.java
@@ -50,41 +50,42 @@ public class crapsFunctions {
     }
 
     public static boolean joueEtGagneUnTourDeCraps(){
-        int score = 0, dice1, dice2, j = 0, goal;
+        int score = 0, tour = 0, dice1, dice2, j = 0, goal;
         boolean back = false;
-        for (int i=0; i < 2;i++){
+        do {
             dice1 = roll();
             dice2 = roll();
             diceDisplay(dice1, dice2);
             score = dice1 + dice2;
-            if (i == 0) {
+            if (tour == 0) {
                 if (score == 7 || score == 11) {
                     System.out.println("Vous avez fait 7 ou 11 au premier coup. Vous avez gagné le gros lot !!");
-                    back = true;i++;
+                    back = true;tour++;// bricolage ?
                 }
                 else if (score == 2 || score == 3 || score == 12) {
-                    System.out.println("Mauvaise pioche pour démarrer, perdu !!!!");i++;
+                    System.out.println("Mauvaise pioche pour démarrer, perdu !!!!");back = true;
                 }
             } else {
                 goal = score;
                 System.out.println("Maintenant l'objectif est : " + goal);
-                while ((score == goal) || (score == 7)) {
+                do {
                     dice1 = roll();
                     dice2 = roll();
                     diceDisplay(dice1, dice2);
                     score = dice1 + dice2;
 
                     if (score == 7) {
-                        System.out.println("You make a 7 and you Loss Man. Try Again.");i++;
+                        System.out.println("You make a 7 and you Loss Man. Try Again.");back = true;
                     }else if (score == goal) {
-                        System.out.println("Vous avez gagné, vous doublé votre mise..");i++;
-                        back = true;
+                        System.out.println("Vous avez gagné, vous doublé votre mise..");back = true;
                     }
                     System.out.println( "Value goal ==> " + goal);
                     System.out.println( "Value score ==> " + score);
-                }
+                }while (back != true);
             }
-        }
+            tour++;
+            System.out.println("value back === > " + back);
+        }while (back != true);
         return back;
     }
 
@@ -105,7 +106,7 @@ public class crapsFunctions {
             }
             answer = askGamer();
             System.out.println("answer == " + answer);
-        }while (mise == 0 || !(answer.equals("N")) || (tokens == 0));
+        }while (mise != 0 && !(answer.equals("N")) && (tokens != 0));
         System.out.println("=========================================");
         System.out.println("==========     GAME OUT       ===========");
         System.out.println("=========================================");
